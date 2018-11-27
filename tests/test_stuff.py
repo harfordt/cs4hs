4bd4398b0800e843468fcf10632793761f8faba8
Tim Harford <tim.harford@wgc.school.nz>
2018-11-28 10:25:28 +1300

    Made dir name plural

---
+++ tests/test_stuff.py
@@ -0,0 +1,15 @@
+from pkg import hello
+
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
+
+def test_power():
+    assert power(2,4) == 16
+    assert power(3,3) == 27
+
+def test_power2():
+    assert power(4,2) == 16
